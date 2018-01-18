496b7e5146bdf5559f60c985116964b99ff35e6b
subpixel <borg.unimatrix0@gmail.com>
2018-01-18 10:43:08 +0000

    starting models

--- app/models/User.php
+++ app/models/User.php
@@ -8,9 +8,56 @@ class User extends Model
 {
     protected $user_id;
     protected $first_name;
+    protected $last_name;
     protected $email;
     protected $username;
     protected $password;
+    
+    // GETTERS
+    
+    public function get_id() {
+        return $this->user_id;
+    }
+    
+    public function get_first() {
+        return $this->first_name;
+    }
+    
+    public function get_last() {
+        return $this->last_name;
+    }
+    
+    public function get_email() {
+        return $this->email;
+    }
+    
+    public function get_username() {
+        return $this->username;
+    }
+    
+    
+    // SETTERS
+    
+    public function set_id($uid) {
+        $this->user_id = $uid;
+    }
+    
+    public function set_first($first) {
+        $this->first = $first;
+    }
+    
+    public function set_last($last) {
+        $this->last = $last;
+    }
+    
+    public function set_email($email) {
+        $this->email = $email;
+    }
+    
+    public function set_password($pwd) {
+        $this->password = $pwd;
+    }
+    
     
     public function validation()
     {
